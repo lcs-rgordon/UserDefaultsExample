6bbc59ceb42c205201889c1f4abc5fabe13c444b
Russell Gordon <rgordon@lcs.on.ca>
2020-11-10 10:28:09 -0500

    Initial Commit

---
+++ UserDefaultsExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserDefaultsExample
+//
+//  Created by Russell Gordon on 2020-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserDefaultsExample/UserDefaultsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsExampleApp.swift
+//  UserDefaultsExample
+//
+//  Created by Russell Gordon on 2020-11-10.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
