092f5947feef94fcff3d2240d996fdd4fce2f5ed
Russell Gordon <rgordon@lcs.on.ca>
2020-11-10 10:33:16 -0500

    Built basic interface to display information about number of times user has opened app.

--- UserDefaultsExample/ContentView.swift
+++ UserDefaultsExample/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // How many times have you been here?
+    @State private var registeredPresenceCount = 0
+    
+    // Have you been here before?
+    @State private var beenHereBefore = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Form {
+                Section(header: Text("Details")) {
+                    if beenHereBefore {
+                        Text("You've been here before")
+                    } else {
+                        Text("Welcome, stranger.")
+                    }
+                    Text("You have been here \(registeredPresenceCount) times before")
+                }
+            }
+        }
     }
 }
 
